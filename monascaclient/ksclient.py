2cf07c0f3b2b2112b5a6294565d79cc46c29d346
d042680 <root@mo-79e9e5fcb.mo.sap.corp>
2015-07-07 07:30:42 +0000

    removing debug code leaking credentials

--- monascaclient/ksclient.py
+++ monascaclient/ksclient.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.i
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -64,7 +64,6 @@ class KSClient(object):
             kc_args['user_domain_id'] = kwargs.get('user_domain_id')
 
         self._kwargs = kwargs
-        log.debug("Creating keystone client: {0}".format(repr(kc_args)))
         self._keystone = client.Client(**kc_args)
         self._token = None
         self._monasca_url = None
