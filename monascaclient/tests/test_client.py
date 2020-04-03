78569fec5bc08db47323a2c6c204fd921bcb0626
Witek Bedyk <witold.bedyk@suse.com>
2020-04-03 16:01:24 +0200

    Fix mock import statements
    As we dropped Python 2 support we don't have to use the third party
    mock library with Python 2 compatibility and can move to Python 3
    standard library. The change updates the import statements to reflect
    that.
    
    Change-Id: I39d9410ae43a2d9e6f6d6eaf74d06a82f2c33524

--- monascaclient/tests/test_client.py
+++ monascaclient/tests/test_client.py
@@ -14,9 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from unittest import mock
 import warnings
 
-import mock
 from oslotest import base
 
 from monascaclient import client
